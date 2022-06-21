7e06e8995c5b5c021774842f4e929e3c1a78c461
thislife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-06-21 10:15:32 +0900

    add gsap

--- main.js
+++ main.js
@@ -166,3 +166,111 @@ window.onload = function () {
     });
   });
 };
+
+var tl = gsap.timeline({ repeat: 6, repeatDelay: 1, yoyo: true });
+tl.to(
+  "h1",
+  { duration: 0.2, className: "+=superShadow", top: "-=10px", ease: "power1.in", stagger: 0.3 },
+  "start"
+);
+
+(function () {
+  var stage;
+  var circles;
+  var colors = [
+    { r: 126, g: 190, b: 197 },
+    { r: 239, g: 143, b: 97 },
+    { r: 106, g: 206, b: 182 },
+    { r: 77, g: 123, b: 125 },
+    { r: 177, g: 157, b: 201 },
+    { r: 168, g: 88, b: 88 },
+    { r: 94, g: 97, b: 97 },
+  ];
+
+  function init() {
+    initStage();
+    initCircles();
+    moveCircles();
+    animate();
+  }
+
+  function initStage() {
+    stage = new createjs.Stage("stage");
+    stage.canvas.width = window.innerWidth;
+    stage.canvas.height = window.innerHeight;
+  }
+
+  function initCircles() {
+    circles = [];
+    var n = (stage.canvas.width * stage.canvas.height) / 700;
+    for (var i = 0; i < n; i++) {
+      var circle = new createjs.Shape();
+      var r = 2 + Math.random() * 6;
+      var x = Math.random() * stage.canvas.width;
+      var y = Math.random() * stage.canvas.height;
+      var color = colors[Math.floor(i % colors.length)];
+      var alpha = 0.2 + Math.random() * 0.5;
+      circle.alpha = alpha;
+      circle.radius = r;
+      circle.graphics
+        .beginFill("rgb(" + color.r + "," + color.g + "," + color.b + ")")
+        .drawCircle(0, 0, r);
+      circle.x = x;
+      circle.y = y;
+      circles.push(circle);
+      stage.addChild(circle);
+    }
+  }
+
+  function animate() {
+    /*createjs.Ticker.addEventListener("tick", handleTick);*/
+    handleTick();
+    requestAnimationFrame(animate);
+  }
+
+  function moveCircles() {
+    for (var i = 0, l = circles.length; i < l; i++) {
+      shiftCircle(circles[i]);
+    }
+  }
+
+  function shiftCircle(c) {
+    var x = -100 + Math.random() * 200;
+    var y = -100 + Math.random() * 200;
+    var delay = Math.random() * 1.5;
+    var radius = 2 + Math.random() * 6;
+    var scale = radius / c.radius;
+    var a = 0.2 + Math.random() * 0.5;
+    TweenLite.to(c, 1.3, {
+      x: c.x + x,
+      y: c.y + y,
+      scaleX: scale,
+      scaleY: scale,
+      alpha: a,
+      ease: Quad.easeInOut,
+      delay: delay,
+      onComplete: function () {
+        shiftCircle(c);
+      },
+    });
+  }
+
+  function handleTick(event) {
+    stage.update();
+  }
+
+  window.onload = function () {
+    init();
+  };
+})();
+
+gsap.to("#circle", {
+  delay: 0.5,
+  duration: 3,
+  stagger: 0.2,
+  y: function (i, elem, boxes) {
+    return i % 2 === 1 ? -50 : 50;
+  },
+  repeat: -1,
+  yoyo: true,
+});
