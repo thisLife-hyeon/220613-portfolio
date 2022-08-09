db806d21c38e3b1bc5fe6503bfe69ded2126bf3f
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-08-09 20:49:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
 const topClass = {
-  home: document.querySelector('.home'),
-  skill: document.querySelector('.skill'),
-  project6: document.querySelector('.project6'),
-  project2: document.querySelector('.project2'),
+  home: document.querySelector(".home"),
+  skill: document.querySelector(".skill"),
+  project7: document.querySelector(".project7"),
+  project2: document.querySelector(".project2"),
   // project3: document.querySelector(".project3"),
-  project5: document.querySelector('.project5'),
-  contact: document.querySelector('.contact'),
-  arrowUp: document.querySelector('.arrow-up'),
+  project5: document.querySelector(".project5"),
+  contact: document.querySelector(".contact"),
+  arrowUp: document.querySelector(".arrow-up"),
 };
 
 const topId = {
-  sectionHome: document.querySelector('#home'),
-  sectionSkill: document.querySelector('#skill'),
-  sectionProject6: document.querySelector('#project6'),
-  sectionProject2: document.querySelector('#project2'),
+  sectionHome: document.querySelector("#home"),
+  sectionSkill: document.querySelector("#skill"),
+  sectionProject7: document.querySelector("#project7"),
+  sectionProject2: document.querySelector("#project2"),
   // sectionProject3: document.querySelector("#project3"),
-  sectionProject5: document.querySelector('#project5'),
-  sectionContact: document.querySelector('#contact'),
+  sectionProject5: document.querySelector("#project5"),
+  sectionContact: document.querySelector("#contact"),
 };
 
-topClass.home.addEventListener('click', function () {
-  topId.sectionHome.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.home.addEventListener("click", function () {
+  topId.sectionHome.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.skill.addEventListener('click', function () {
-  topId.sectionSkill.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.skill.addEventListener("click", function () {
+  topId.sectionSkill.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.project5.addEventListener('click', function () {
-  topId.sectionProject5.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.project5.addEventListener("click", function () {
+  topId.sectionProject5.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.project6.addEventListener('click', function () {
-  topId.sectionProject6.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.project7.addEventListener("click", function () {
+  topId.sectionProject7.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.project2.addEventListener('click', function () {
-  topId.sectionProject2.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.project2.addEventListener("click", function () {
+  topId.sectionProject2.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.contact.addEventListener('click', function () {
-  topId.sectionContact.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.contact.addEventListener("click", function () {
+  topId.sectionContact.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-topClass.arrowUp.addEventListener('click', function () {
-  topId.sectionHome.scrollIntoView({ behavior: 'smooth', block: 'center' });
+topClass.arrowUp.addEventListener("click", function () {
+  topId.sectionHome.scrollIntoView({ behavior: "smooth", block: "center" });
 });
 
-document.addEventListener('scroll', function () {
+document.addEventListener("scroll", function () {
   if (window.scrollY > 300) {
-    topClass.arrowUp.classList.add('show');
+    topClass.arrowUp.classList.add("show");
   } else {
-    topClass.arrowUp.classList.remove('show');
+    topClass.arrowUp.classList.remove("show");
   }
 });
 
-const boxes = document.querySelectorAll('.active__box');
+const boxes = document.querySelectorAll(".active__box");
 
-window.addEventListener('scroll', checkBoxes);
+window.addEventListener("scroll", checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
@@ -61,9 +61,9 @@ function checkBoxes() {
     const boxTop = box.getBoundingClientRect().top;
 
     if (boxTop < triggerBottom) {
-      box.classList.add('show');
+      box.classList.add("show");
     } else {
-      box.classList.remove('show');
+      box.classList.remove("show");
     }
   });
 }
@@ -71,19 +71,19 @@ function checkBoxes() {
 //Scroll Magic Start
 
 // define images
-var images = ['img/bg-1.png', 'img/bg-2.png', 'img/bg-3.png', 'img/bg-4.png', 'img/bg-5.png'];
+var images = ["img/bg-1.png", "img/bg-2.png", "img/bg-3.png", "img/bg-4.png", "img/bg-5.png"];
 // TweenMax can tween any property of any object. We use this object to cycle through the array
 var obj = { curImg: 0 };
 
 // create tween
 var tween = TweenMax.to(obj, 0.5, {
   curImg: images.length - 1, // animate propery curImg to number of images
-  roundProps: 'curImg', // only integers so it can be used as an array index
+  roundProps: "curImg", // only integers so it can be used as an array index
   repeat: 1, // repeat 3 times
   immediateRender: true, // load first image automatically
   ease: Linear.easeNone, // show every image the same ammount of time
   onUpdate: function () {
-    $('#myimg').attr('src', images[obj.curImg]); // set the image source
+    $("#myimg").attr("src", images[obj.curImg]); // set the image source
   },
 });
 
@@ -91,24 +91,24 @@ var tween = TweenMax.to(obj, 0.5, {
 var controller = new ScrollMagic.Controller();
 
 // build scene
-var scene = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 800 })
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger", duration: 800 })
   .setTween(tween)
   // .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 
 // handle form change
-$('form.move input[name=duration]:radio').change(function () {
+$("form.move input[name=duration]:radio").change(function () {
   scene.duration($(this).val());
 });
 //Scroll Magic End
 
 //Swiper
 // Initialize Swiper
-var swiper = new Swiper('.mySwiper', {
-  effect: 'coverflow',
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   autoplay: true,
   loop: true,
   coverflowEffect: {
@@ -119,16 +119,16 @@ var swiper = new Swiper('.mySwiper', {
     slideShadows: true,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 });
 //Swiper End
 
 window.onload = function () {
-  const elm = document.querySelectorAll('.section');
+  const elm = document.querySelectorAll(".section");
   const elmCount = elm.length;
   elm.forEach(function (item, index) {
-    item.addEventListener('mousewheel', function (event) {
+    item.addEventListener("mousewheel", function (event) {
       event.preventDefault();
       let delta = 0;
 
@@ -145,7 +145,8 @@ window.onload = function () {
       if (delta < 0) {
         if (elmSelector !== elmCount - 1) {
           try {
-            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+            moveTop =
+              window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
           } catch (e) {}
         }
       }
@@ -154,20 +155,25 @@ window.onload = function () {
       else {
         if (elmSelector !== 0) {
           try {
-            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+            moveTop =
+              window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
           } catch (e) {}
         }
       }
 
-      const body = document.querySelector('html');
-      window.scrollTo({ top: moveTop, left: 0, behavior: 'smooth' });
+      const body = document.querySelector("html");
+      window.scrollTo({ top: moveTop, left: 0, behavior: "smooth" });
     });
   });
 };
 
 //상단 h1글자 3D느낌
 var tl = gsap.timeline({ repeat: 6, repeatDelay: 1, yoyo: true });
-tl.to('h1', { duration: 0.2, className: '+=superShadow', top: '-=10px', ease: 'power1.in', stagger: 0.3 }, 'start');
+tl.to(
+  "h1",
+  { duration: 0.2, className: "+=superShadow", top: "-=10px", ease: "power1.in", stagger: 0.3 },
+  "start"
+);
 
 (function () {
   var stage;
@@ -208,7 +214,9 @@ tl.to('h1', { duration: 0.2, className: '+=superShadow', top: '-=10px', ease: 'p
       var alpha = 0.2 + Math.random() * 0.5;
       circle.alpha = alpha;
       circle.radius = r;
-      circle.graphics.beginFill('rgb(' + color.r + ',' + color.g + ',' + color.b + ')').drawCircle(0, 0, r);
+      circle.graphics
+        .beginFill("rgb(" + color.r + "," + color.g + "," + color.b + ")")
+        .drawCircle(0, 0, r);
       circle.x = x;
       circle.y = y;
       circles.push(circle);
@@ -258,7 +266,7 @@ tl.to('h1', { duration: 0.2, className: '+=superShadow', top: '-=10px', ease: 'p
   // };
 })();
 
-gsap.to('#circle', {
+gsap.to("#circle", {
   delay: 0.5,
   duration: 3,
   stagger: 0.2,
